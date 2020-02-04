dc185476fcbcd6a0419f2306530f8297c2f62f87
cfriday99 <ccfriday99@gmail.com>
2020-02-03 21:32:30 -0500

    Added Project

---
+++ Mega Man/Assets/MovementScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class MovementScript : MonoBehaviour
+{
+
+    public float movementSpeed = 5.0f;
+    public float jumpForce = 300.0f;
+    public LayerMask whatIsGround;
+
+    private Rigidbody2D rb;
+    private float horizontalMovement;
+    private bool jump;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        horizontalMovement = Input.GetAxisRaw("Horizontal") * movementSpeed;
+        if (Physics2D.OverlapCircle(transform.position - new Vector3(0.0f, 0.5f, 0.0f), 0.1f, whatIsGround)) 
+            jump = Input.GetKeyDown(KeyCode.Space);
+    }
+
+    private void FixedUpdate()
+    {
+        if (jump)
+            rb.AddForce(new Vector2(0.0f, jumpForce));
+            rb.velocity = new Vector2(horizontalMovement, rb.velocity.y);
+            jump = false;
+    }
+
+}
---
+++ Mega Man/Assets/PlayerController2D.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController2D : MonoBehaviour
+{
+    private Rigidbody2D rb2d;
+    private SpriteRenderer spriteRenderer;
+
+    public float speed;
+    public float jumpForce;
+
+    bool isGrounded;
+
+    public Transform groundCheck;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        if (Physics2D.Linecast(transform.position, groundCheck.position, 1 << LayerMask.NameToLayer("Ground")))
+        {
+            isGrounded = true;
+        }
+        else
+        {
+            isGrounded = false;
+        }
+
+        if (Input.GetKey("d") || Input.GetKey("right"))
+        {
+            rb2d.velocity = new Vector2(speed, rb2d.velocity.y);
+            {
+                if (isGrounded)
+                {
+                    spriteRenderer.flipX = false;
+                }
+            }
+        }
+        else if (Input.GetKey("a") || Input.GetKey("left"))
+        {
+            rb2d.velocity = new Vector2(-speed, rb2d.velocity.y);
+            {
+                if (spriteRenderer != null)
+                {
+                    if (isGrounded)
+                    {
+                        spriteRenderer.flipX = true;
+                    }
+                }
+            }
+        }
+        else
+        {
+            //animator.Play("Player_idle");
+            //if (isGrounded)
+            //{
+                rb2d.velocity = new Vector2(0, rb2d.velocity.y);
+            //}
+        }
+        if (Input.GetKey("w") && isGrounded || Input.GetKey("up") && isGrounded)
+        {
+            //animator.Play("Player_jump");
+            rb2d.velocity = new Vector2(rb2d.velocity.x, jumpForce);
+
+        }
+        if (Input.GetKey("escape"))
+            Application.Quit();
+    }
+}
