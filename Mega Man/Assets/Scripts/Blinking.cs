b15769f6fb9fbad0745cfb8650155dc4624ca9bc
cfriday99 <ccfriday99@gmail.com>
2020-02-06 22:56:03 -0500

    Main Menu Update

---
+++ Mega Man/Assets/Scripts/Blinking.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Blinking : MonoBehaviour
+{
+    public MaskableGraphic imageToToggle;
+
+    public float interval = 1f;
+    public float startDelay = 0.5f;
+    public bool currentState = true;
+    public bool defaultState = true;
+    bool isBlinking = false;
+
+    void Start()
+    {
+        imageToToggle.enabled = defaultState;
+        StartBlink();
+    }
+
+    public void StartBlink()
+    {
+        if (isBlinking)
+            return;
+
+        if (imageToToggle != null)
+        {
+            isBlinking = true;
+            InvokeRepeating("ToggleState", startDelay, interval);
+        }
+    }
+
+    public void ToggleState()
+    {
+        imageToToggle.enabled = !imageToToggle.enabled;
+
+    }
+
+}
---
+++ Mega Man/Assets/Scripts/PlayerController2D.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController2D : MonoBehaviour
+{
+    private Rigidbody2D rb2d;
+    private SpriteRenderer spriteRenderer;
+
+    public Animator animator;
+    public GameObject bullet;
+    public float speed;
+    public float jumpForce;
+
+    bool isGrounded;
+
+    public Transform groundCheck;
+
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            GameObject b = (GameObject)(Instantiate(bullet, transform.position + transform.forward * 1.5f, Quaternion.identity));
+
+            b.GetComponent<Rigidbody2D>().AddForce(transform.forward * 1000);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (Physics2D.Linecast(transform.position, groundCheck.position, 1 << LayerMask.NameToLayer("Ground")))
+        {
+            isGrounded = true;
+        }
+        else
+        {
+            isGrounded = false;
+        }
+
+        if (Input.GetKey("d") || Input.GetKey("right"))
+        {
+            rb2d.velocity = new Vector2(speed, rb2d.velocity.y);
+            {
+                if (isGrounded)
+                {
+                    animator.SetFloat("Speed", speed);
+                    spriteRenderer.flipX = false;
+                }
+            }
+        }
+        else if (Input.GetKey("a") || Input.GetKey("left"))
+        {
+            rb2d.velocity = new Vector2(-speed, rb2d.velocity.y);
+            {
+                if (spriteRenderer != null)
+                {
+                    if (isGrounded)
+                    {
+                        animator.SetFloat("Speed", speed);
+                        spriteRenderer.flipX = true;
+                    }
+                }
+            }
+        }
+        else
+        {
+            //animator.Play("Player_idle");
+            //if (isGrounded)
+            //{
+                animator.SetFloat("Speed", 0);
+                rb2d.velocity = new Vector2(0, rb2d.velocity.y);
+            //}
+        }
+        if (Input.GetKey("w") && isGrounded || Input.GetKey("up") && isGrounded)
+        {
+            //animator.Play("Player_jump");
+            rb2d.velocity = new Vector2(rb2d.velocity.x, jumpForce);
+
+        }
+        if (Input.GetKey("escape"))
+            Application.Quit();
+    }
+
+    //Friction For Moving Platforms
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag.Equals("Platform"))
+            this.transform.parent = collision.transform;
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag.Equals("Platform"))
+            this.transform.parent = null;
+    }
+}
